luoboton/fcitx

commit 76250f0fef91311217e58c91900bbbf34da4e830

[preedit_blacklist] use FcitxInputContext2 instead

src/frontend/ipc/ipc.c
@@ -451,7 +451,7 @@ void IPCCreateIC(void* arg, FcitxInputContext* context, void* priv)
                                  DBUS_TYPE_UINT32, &arg4,
                                  DBUS_TYPE_INVALID);
     }
-    context->prgname = ipcic->appname;
+    ((FcitxInputContext2*)context)->prgname = ipcic->appname;
     dbus_connection_send(ipcpriv->conn, reply, NULL);
     dbus_message_unref(reply);
 

src/lib/fcitx/frontend.c
@@ -168,7 +168,7 @@ FcitxInstanceCreateIC(FcitxInstance* instance, int frontendid, void * priv)
     rec->frontendid = frontendid;
     rec->offset_x = -1;
     rec->offset_y = -1;
-    rec->prgname = NULL;
+    ((FcitxInputContext2*)rec)->prgname = NULL;
 
     NewICData(instance, rec);
     switch (instance->config->shareState) {
@@ -594,7 +594,7 @@ static boolean AppPreeditBlacklisted(FcitxInputContext* ic)
         }
     }
 
-    const char* prgname = ic->prgname;
+    const char* prgname = ((FcitxInputContext2*)ic)->prgname;
     if (!prgname)
         return false;
 

src/lib/fcitx/frontend.h
@@ -89,7 +89,6 @@ extern "C" {
         int offset_y; /**< y offset to the window */
         int frontendid; /**< frontend id */
         void *privateic; /**< private input context data */
-        char *prgname; /**< program name */
         FcitxCapacityFlags contextCaps; /**< input context capacity */
         struct _FcitxInputContext* next; /**< next input context */
     } FcitxInputContext;
@@ -99,7 +98,8 @@ extern "C" {
         char* imname;
         intptr_t switchBySwitchKey;
         UT_array* data;
-        void* padding[14];
+        char* prgname; /**< program name */
+        void* padding[13];
     } FcitxInputContext2;
 
     /**

commit 90674f5dcd9a0aafd7669f756d79bfa17be5bd72

[ipc] fix share state bug introduced by commit 76250f0fef
FcitxInstanceSetICStateFromSameApplication uses prgname before
it is set correct. Need to set prgname before that.

src/frontend/ipc/ipc.c
@@ -373,7 +373,6 @@ void IPCCreateIC(void* arg, FcitxInputContext* context, void* priv)
     sprintf(ipcic->path, FCITX_IC_DBUS_PATH, ipcic->id);
 
     uint32_t arg1, arg2, arg3, arg4;
-    char* appname = NULL;
     arg1 = config->hkTrigger[0].sym;
     arg2 = config->hkTrigger[0].state;
     arg3 = config->hkTrigger[1].sym;
@@ -393,6 +392,7 @@ void IPCCreateIC(void* arg, FcitxInputContext* context, void* priv)
 
         DBusError error;
         dbus_error_init(&error);
+        char* appname = NULL;
         if (!dbus_message_get_args(message, &error, DBUS_TYPE_STRING, &appname, DBUS_TYPE_INVALID))
             appname = NULL;
         else {
@@ -402,9 +402,11 @@ void IPCCreateIC(void* arg, FcitxInputContext* context, void* priv)
                 appname = strdup(appname);
             }
         }
+        ((FcitxInputContext2*)context)->prgname = appname;
 
-        if (config->shareState == ShareState_PerProgram)
+        if (config->shareState == ShareState_PerProgram) {
             FcitxInstanceSetICStateFromSameApplication(ipc->owner, ipc->frontendid, context);
+        }
 
         boolean arg0 = context->state != IS_CLOSED;
 
@@ -422,6 +424,7 @@ void IPCCreateIC(void* arg, FcitxInputContext* context, void* priv)
         DBusError error;
         dbus_error_init(&error);
         int icpid = 0;
+        char* appname = NULL;
         if (!dbus_message_get_args(message, &error, DBUS_TYPE_STRING, &appname, DBUS_TYPE_INT32, &icpid, DBUS_TYPE_INVALID))
             appname = NULL;
         else {
@@ -432,9 +435,11 @@ void IPCCreateIC(void* arg, FcitxInputContext* context, void* priv)
             }
         }
         ipcic->pid = icpid;
+        ((FcitxInputContext2*)context)->prgname = appname;
 
-        if (config->shareState == ShareState_PerProgram)
+        if (config->shareState == ShareState_PerProgram) {
             FcitxInstanceSetICStateFromSameApplication(ipc->owner, ipc->frontendid, context);
+        }
 
         boolean arg0 = context->state != IS_CLOSED;
 
@@ -448,7 +453,6 @@ void IPCCreateIC(void* arg, FcitxInputContext* context, void* priv)
                                  DBUS_TYPE_UINT32, &arg4,
                                  DBUS_TYPE_INVALID);
     }
-    ((FcitxInputContext2*)context)->prgname = appname;
     dbus_connection_send(ipcpriv->conn, reply, NULL);
     dbus_message_unref(reply);
 